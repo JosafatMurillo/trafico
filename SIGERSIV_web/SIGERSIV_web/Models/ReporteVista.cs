e7022410435304e18332f13a9027648d54e1b42b
AdolfoA98 <adolfo_a1513@hotmail.com>
2019-06-15 16:54:41 -0500

    Front-end de la aplicación web

---
+++ SIGERSIV_web/SIGERSIV_web/Models/ReporteVista.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIGERSIV.Models
+{
+    public class ReporteVista
+    {
+        public String Lugar { get; set; }
+        public String Cliente { get; set; }
+        public String EstatusReporte { get; set; }
+    }
+}
